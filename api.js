996b2ce50c879f904863850a86e62f8468d7bee1
Jean-678 <jeanbaptist.malcekan@univ.edu.vu>
2026-05-20 15:39:39 +1100

    mistake commit

--- api.js
+++ api.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Application deployed successfully');
 });
 
+app.get('/api', (req, res) => {
+  res.sen('New API');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
